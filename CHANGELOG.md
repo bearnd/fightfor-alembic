36e9e612a0be58474cbad267812978d20f8aeb43
Adamos Kyriakou <somada141@gmail.com>
2018-06-12 20:34:38 +1000

    Bumped version and updated `CHANGELOG.md`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### v0.1.1
+
+- Added new revision to add the supplemental-synonyms table.
+
 ### v0.1.0
 
 - Initial release.
--- ffalembic/__init__.py
+++ ffalembic/__init__.py
@@ -1,1 +1,10 @@
 # coding=utf-8
+
+"""Top-level package for fightfor-alembic."""
+
+__author__ = """Adamos Kyriakou"""
+__email__ = 'somada141@gmail.com'
+__version__ = '0.1.1'
+
+from ffalembic import config
+from ffalembic import excs
